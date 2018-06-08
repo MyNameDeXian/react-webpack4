14e30c5fa1ae940421bbe6e33fb2eb5df0dcbf95
dexianFree <517334881@qq.com>
2018-06-08 09:10:13 +0800

    repair bud

--- webpack/webpack.pro.js
+++ webpack/webpack.pro.js
@@ -1,6 +1,5 @@
 const path = require('path')
 const webpack = require('webpack')
-const config = require('config')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
 
