307a6da85c4facbdef8f72af3dd1654d6b55fcdb
dexianFree <517334881@qq.com>
2018-06-14 11:26:33 +0800

    add header tabs

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,24 +2,57 @@ import React, { Component } from 'react'
 import './header.scss'
 import Input from '@material-ui/core/Input'
 import Button from '@material-ui/core/Button'
+import Paper from '@material-ui/core/Paper';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Search from '@material-ui/icons/Search';
+import IconButton from '@material-ui/core/IconButton';
+import InputAdornment from '@material-ui/core/InputAdornment';
 
 class Header extends Component {
 	constructor(props){
 		super(props);
 		this.state = {
-
+			value: false
 		}
 	}
 	render(){
 		let { focus } = this.state;
+		// fullWidth centered scrollable
 		return(
-			<header className='header-comp flex-row f-ai-c f-jc-sa'>
-	        	<Button variant="outlined" color="primary" onClick={this.clickBtn}>
-	          	To Address
-	        	</Button>
-				<Input placeholder='search'/>
+			<header className='header-comp flex-row f-ai-c'>
+				<Tabs
+					className='f-1'
+	          	value={this.state.value}
+	          	onChange={this.handleChange}
+	          	indicatorColor="primary"
+	          	textColor="primary"
+	          	scrollable 
+	          	scrollButtons="auto"
+          	>
+	          	<Tab label="home" />
+	          	<Tab label="plan" />
+	          	<Tab label="web learning" />
+	          	<Tab label="website" />
+	        	</Tabs>
+				<Input placeholder='search'
+					endAdornment={
+						<InputAdornment position='end'>
+							<IconButton
+		                  aria-label="search"
+		                  onClick={this.handleClickShowPassword}
+		               >
+		                  <Search/>
+		               </IconButton>
+						</InputAdornment>
+            }/>
 			</header>
 		)
+	}
+	handleChange = (e, activeTab) =>{
+		this.setState({
+			value: activeTab
+		})
 	}
 	setSearchBox = () =>{
 		let { focus } = this.state;
